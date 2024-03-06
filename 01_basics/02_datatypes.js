c9fd2128b1f42098d73b0bb333ae6e18c84a1b6e
Girish M <77891310+Girish2507@users.noreply.github.com>
2024-03-06 10:08:37 +0000

    Completed the datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treats all the JS Code as the newer version of the JS
+
+//alert("Hello") // this is used in the browser mainly but now we can use in the node as well
+// we are using the node js but not the browser
+
+/*Having the code readiablity should be high 
+    og docs for the JS -> https://tc39.es/ecma262/ or the MDN docs 
+*/
+
+let name ="Girish"
+let age = 18
+let isLoggedIn = true
+
+/*
+number => 2 to the power 53(max) , if the number is too big, we can use bigInt
+string => this is the collection of characters, used with " " or '';
+boolean => true or false
+null => standalone value , this means the variable value is empty , it is a object to be precise
+undefined => when the value is not defined for a variable
+symbol => used to identify the uniqueness of the components
+object => 
+*/
+console.log(typeof null); // type => object
+console.log(typeof undefined); //type=>undefined
